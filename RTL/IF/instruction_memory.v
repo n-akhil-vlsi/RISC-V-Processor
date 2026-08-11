a3cb036cbdb8a275e6b46d22bb4b1fdc6badbe25
AKHIL.N <nalamasuakhil@gmail.com>
2026-08-11 23:11:42 +0530

    added the IF files

---
+++ RTL/IF/instruction_memory.v
@@ -0,0 +1,17 @@
+module instruction_memory (
+
+    input  wire [31:0] address,
+    output wire [31:0] instruction
+
+);
+
+    // 64 x 32-bit Instruction Memory
+    reg [31:0] memory [0:63];
+
+    initial begin
+        $readmemh("Memory/program.mem", memory);        //It loads the hexadecimal instructions from Memory/program.mem into the memory array when simulation starts.
+    end
+
+    assign instruction = memory[address[31:2]];     //each instruction is 4 bytes, the instructions are stored at addresses 0, 4, 8, 12, 16, .... Therefore, the lowest 2 address bits are always 00.
+
+endmodule
